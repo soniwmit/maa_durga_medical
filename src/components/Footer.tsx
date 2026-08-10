094f1e38dfb578e4f218ff3af5efec44782e0845
soniwmit <soni.wmit@gmail.com>
2026-08-10 15:56:23 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
   const [modalType, setModalType] = useState<'privacy' | 'terms' | 'disclaimer' | null>(null);
 
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
